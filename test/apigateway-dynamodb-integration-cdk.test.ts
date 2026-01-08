a0260a3fd4d58e39a68bfbfe0b995e8ee6ecc5bd
Marcio Cruz de Almeida <marciocadev@gmail.com>
2026-01-08 17:31:39 -0300

    Initial commit

---
+++ test/apigateway-dynamodb-integration-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ApigatewayDynamodbIntegrationCdk from '../lib/apigateway-dynamodb-integration-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/apigateway-dynamodb-integration-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ApigatewayDynamodbIntegrationCdk.ApigatewayDynamodbIntegrationCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
